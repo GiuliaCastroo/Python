e535a2ce8dd8df3fd6e6fb7f1266e5ea0a75aecd
GiuliaCastroo <maju.nogueir3.castr3@gmail.com>
2024-05-12 21:06:10 -0300

    Add projetos

--- Jornada Python/Power Up/Python Powerup/auxiliar.py
+++ Jornada Python/Power Up/Python Powerup/auxiliar.py
@@ -2,5 +2,23 @@ import pyautogui
 import time
 
 
-time.sleep (5)
-print(pyautogui.position ())
+time.sleep (5) #da o tempo necesssário para eu abrir a janela e posicionar o mouse
+print(pyautogui.position ()) #Captura a posição da tela que eu precis
+
+
+
+
+"""       codigo       marca        tipo  categoria  preco_unitario  custo               obs
+0    MOLO000251    Logitech       Mouse          1           25.95    6.5               NaN
+1    MOLO000192    Logitech       Mouse          2           19.95    5.0               NaN
+2    CAHA000251     Hashtag      Camisa          1           25.00   11.0               NaN
+3    CAHA000252     Hashtag      Camisa          2           25.00   11.0  Conferir estoque
+4    MOMU000111  Multilaser       Mouse          1           11.99    3.4               NaN
+..          ...         ...         ...        ...             ...    ...               ...
+288  ACAP000192       Apple  Acessorios          2           19.00    3.8               NaN
+289  ACSA0009.3     Samsung  Acessorios          3            9.55    2.1               NaN
+290  CEMO000271    Motorola     Celular          1          279.00   72.5               NaN
+291  FOMO000152    Motorola        Fone          2          150.00   33.0               NaN
+292  CEMO000223    Motorola     Celular          3          229.00   55.0               NaN
+
+[293 rows x 7 columns]"""
--- Jornada Python/Power Up/Python Powerup/codigo.py
+++ Jornada Python/Power Up/Python Powerup/codigo.py
@@ -3,7 +3,7 @@
 #https://dlp.hashtagtreinamentos.com/python/intensivao/login
 #Passo 2: Fazer Login
 #Passo 3: Importar a base de dados
-#Passo 4: Casdastrar um produto
+#Passo 4: Cadastrar um produto
 #Passo 5: Enviar
 #Passo 6: Cadastrar produtos até acabar
 
@@ -12,37 +12,67 @@
     #pyautogui.hotkey (ex. ctrl + alt) para combinações de teclas
 ######################################################################################
 
-# Importar bibliotecas
 import pyautogui
-import time 
+import time
+import pandas as pd
 
-pyautogui.PAUSE = 1.5
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.hotkey -> combinação de teclas
+pyautogui.PAUSE = 1.0
 
-#variaveis
-link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-navegador = "Opera"
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("Opera")
+pyautogui.press("enter")
 
-# Passo 1: Entrar no sistema da empresa (Abrir o navegador)
-
-pyautogui.press ("win")
-pyautogui.write (navegador)
-pyautogui.press ("enter")
-
-pyautogui.write (link)
-pyautogui.press ("enter")
-
-#Dar uma pausa em um lugar especifico
+# entrar no link 
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
 time.sleep(3)
 
 
-#Passo 2: Fazer Login
-pyautogui.click (x=661, y=487)
-pyautogui.write ("adm@teste.com")
-pyautogui.press ("tab")
-pyautogui.write ("adm123")
+# Passo 2: Fazer login
+# selecionar o campo de email
+pyautogui.click(x=806, y=481)
+# escrever o seu email
+pyautogui.write("adm@teste.com.br")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("sua senha")
+pyautogui.press("tab") # clique no botao de login
+pyautogui.press("enter")
 
-#logar no botão de logar
-pyautogui.click (x=987, y=692)
-time.sleep(3) #garantia para o site carregar
 
-#Passo 3: Importar a base de dados
+# Passo 3: Importar a base de produtos pra cadastrar
+
+
+tabela = pd.read_csv("produtos.csv")
+
+print(tabela)
+
+# Passo 4: Cadastrar um produto
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=779, y=346)
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    pyautogui.scroll(5000)
+    # Passo 5: Repetir o processo de cadastro até o fim
