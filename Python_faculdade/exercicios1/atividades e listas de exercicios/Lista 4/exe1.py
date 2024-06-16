afb6dc32b66fe1e53fdf9422f3478e6d2441b6d9
GiuliaCastroo <maju.nogueir3.castr3@gmail.com>
2024-06-16 18:06:21 -0300

    Adicionando projeto de restaurantes

---
+++ Python_faculdade/exercicios1/atividades e listas de exercicios/Lista 4/exe1.py
@@ -0,0 +1,5 @@
+"""
+1) Elaborar um algoritmo que leia um valor de temperatura em graus Celsius, calcule e exiba a
+temperatura equivalente em Kelvin, sabendo que: K = C + 273. Esse algoritmo deve repetir 5 vezes.
+"""
+
---
+++ Python_faculdade/exercicios1/atividades e listas de exercicios/Lista6/exe1.py
@@ -0,0 +1,15 @@
+"""1) Elaborar um programa que receba dois números. Calcule e mostre:
+a) a soma dos números pares desse intervalo entre os dois números, incluindo os números
+digitados.
+b) a multiplicação dos números ímpares desse intervalo, incluindo os digitados."""
+
+
+lista_pares = []
+lista_impares = []
+
+num1= float(input("Digite um número: "))
+num2= float(input("Digite um número: "))
+
+if num1
+
+
