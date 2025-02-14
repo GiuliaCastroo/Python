526fcb444057c2fd2fbc96098286b0245453c9d5
GiuliaCastroo <maju.nogueir3.castr3@gmail.com>
2025-02-14 17:07:22 -0300

    Adicionando aplicativo atualizado

--- Python_main/ALURA/delivery/app.py
+++ Python_main/ALURA/delivery/app.py
@@ -1,9 +1,75 @@
-print("Sabor Express\n")
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False}, 
+                {'nome': 'Pizzaria são josé', 'categoria': 'Pizza', 'ativo': True},
+                {'nome': 'Cantina', 'categoria': 'Italiana', 'ativo': False}]
 
-opcao_escolhida = input('Escolha uma opção: ')
-print("Você escolheu a opção", opcao_escolhida)
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalizando o app')
+
+def voltar_menu():
+    input ('\nDigite uma tecla para voltar ao menu principal.')
+    main()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    exibir_subtitulo('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    categoria = input(f'Digite a categoria do {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome' : nome_do_restaurante, 'categoria' : categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_menu()
+
+def listar_restaurantes():
+    os.system('cls')
+    exibir_subtitulo('Listando restaurantes cadastrados\n')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria_restaurante = restaurante['categoria']
+        ativo_restaurante = restaurante['ativo']
+        print(f'-{nome_restaurante} | {categoria_restaurante} | {ativo_restaurante}')
+
+    voltar_menu()
+
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1: 
+        cadastrar_novo_restaurante()
+    elif opcao_escolhida == 2: 
+        listar_restaurantes()
+    elif opcao_escolhida == 3: 
+        print('Ativar restaurante')
+    else: 
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
