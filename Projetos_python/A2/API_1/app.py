9d0fdd5d5383c8514c582422054b266a822c62f2
GiuliaCastroo <maju.nogueir3.castr3@gmail.com>
2025-05-08 17:13:27 -0300

    Adicionando API de Python

---
+++ Projetos_python/A2/API_1/app.py
@@ -0,0 +1,76 @@
+'''
+1- Objetivo - Criar uma API que disponibiliza a consulta, criação, edição e exclusão de livros
+2- URL base
+3- Endpoints - O que minha API vai disponibilizar 
+4- Quais recursos
+
+'''
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+livros = [
+    {
+        'id' : 1,
+        'titulo':'Dracula',
+        'autor':'Bram Stoker'
+    },
+    
+    {
+        'id' : 2,
+        'titulo':'Carmilla',
+        'autor':'Joseph Sheridan Le Fanu'
+    },
+
+    {
+        'id' : 3,
+        'titulo':'Wuthering Heights',
+        'autor':'Emily Brontë'
+    },
+    
+    ]
+
+
+#Vamos criar uma função para cada Endpoint
+
+#Consultar (Todos)
+@app.route('/livros', methods = ['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+#Consultar (id)
+@app.route('/livros/<int:id>', methods = ['GET'])
+def obter_livro_por_id (id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+#Editar
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+
+#Criar 
+@app.route('/livros', methods= ['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    
+    return jsonify(livros)
+    
+#Excluir
+@app.route('/livros/<int:id>', methods =['DELETE'])
+def excluir_livros(id):
+    for indice, livro in enumerate(livros):
+        if livro.get ('id') == id:
+            del livro[indice]
+
+    return jsonify(livros)
+
+
+app.run(port=5000, host= 'localhost', debug= True)
